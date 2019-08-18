ac85fac93882d30cdb7a096acb0d2d339bc691ac
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-18 22:13:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,10 @@
 #   end 
 #   return new_sentence.join(" ")
 # end 
+
+def reverse_each_word(sentence)
+  sentence_array = sentence.split(" ")
+  sentence_array.collect do |word|
+    "#{word.reverse.join(" ")}"
+  end 
+end 
