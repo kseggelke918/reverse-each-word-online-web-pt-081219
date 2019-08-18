275d9bf33f167d407576f7ee8ffb557f8fe2fa34
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-18 22:11:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
 def reverse_each_word(sentence)
+  new_sentence = []
   sentence_array = sentence.split(" ")
-  new_sentence = sentence_array.each do |word|
-    "#{word.reverse}"
+  sentence_array.each do |word|
+    new_sentence << "#{word.reverse}"
   end 
+  return new_sentence.join
 end 
